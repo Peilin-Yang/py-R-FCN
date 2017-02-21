dba286177519b0ecd41ff435928bc0f524d47739
Peilin Yang <yangpeilyn@gmail.com>
2017-02-21 08:47:28 -0500

    add end2end detect

---
+++ experiments/scripts/rfcn_end2end_detect.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#output/rfcn_end2end_ohem/bib_500X500_training/resnet101_rfcn_ohem_iter_70000.caffemodel \
+time ./tools/detect_net.py --gpu 0 \
+  --def models/bib/ResNet-101/rfcn_end2end/test_agnostic.prototxt \
+  --net $1 \
+  --cfg experiments/cfgs/rfcn_end2end.yml \
+  --input $2 \
+  --output $3 \
+  --excludes $4
